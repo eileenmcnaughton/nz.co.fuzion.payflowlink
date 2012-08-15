c1e29dd6875d9cd3ed85585ce96382dd303ef674
eileen <eileen@fuzion.co.nz>
2012-08-15 16:05:19 +1200

    fixes for 4.2 ipn integration - can be run with my backported 4.1

--- PayflowLink.php
+++ PayflowLink.php
@@ -26,7 +26,7 @@
 /*
  * Payment processor plugin for Payflow Link
  */
- 
+
 require_once 'CRM/Core/Payment.php';
 
 
@@ -36,15 +36,15 @@ class nz_co_fuzion_payflowlink extends CRM_Core_Payment {
     static protected $_mode = null;
 
     static protected $_params = array();
-       /** 
-    * We only need one instance of this object. So we use the singleton 
-    * pattern and cache the instance in this variable 
-    * 
-    * @var object 
-    * @static 
-    */ 
-   static private $_singleton = null; 
-   
+       /**
+    * We only need one instance of this object. So we use the singleton
+    * pattern and cache the instance in this variable
+    *
+    * @var object
+    * @static
+    */
+   static private $_singleton = null;
+
     /**
      * Constructor
      *
@@ -58,15 +58,15 @@ class nz_co_fuzion_payflowlink extends CRM_Core_Payment {
         $this->_paymentProcessor = $paymentProcessor;
         $this->_processorName    = ts('PayflowLink');
     }
-   /** 
-     * singleton function used to manage this object 
-     * 
+   /**
+     * singleton function used to manage this object
+     *
      * @param string $mode the mode of operation: live or test
      *
-     * @return object 
-     * @static 
-     * 
-     */ 
+     * @return object
+     * @static
+     *
+     */
     static function &singleton( $mode, &$paymentProcessor ) {
         $processorName = $paymentProcessor['name'];
         if (self::$_singleton[$processorName] === null ) {
@@ -74,7 +74,7 @@ class nz_co_fuzion_payflowlink extends CRM_Core_Payment {
         }
         return self::$_singleton[$processorName];
     }
-    
+
     function checkConfig( ) {
         $config = CRM_Core_Config::singleton( );
 
@@ -83,11 +83,11 @@ class nz_co_fuzion_payflowlink extends CRM_Core_Payment {
         if ( empty( $this->_paymentProcessor['user_name'] ) ) {
             $error[] = ts( 'UserID is not set in the Administer CiviCRM &raquo; Payment Processor.' );
         }
-        
+
         if ( empty( $this->_paymentProcessor['password'] ) ) {
             $error[] = ts( 'password is not set in the Administer CiviCRM &raquo; Payment Processor.' );
         }
-        
+
         if ( ! empty( $error ) ) {
             return implode( '<p>', $error );
         } else {
@@ -96,51 +96,51 @@ class nz_co_fuzion_payflowlink extends CRM_Core_Payment {
     }
 
     function setExpressCheckOut( &$params ) {
-        CRM_Core_Error::fatal( ts( 'This function is not implemented' ) ); 
+        CRM_Core_Error::fatal( ts( 'This function is not implemented' ) );
     }
     function getExpressCheckoutDetails( $token ) {
-        CRM_Core_Error::fatal( ts( 'This function is not implemented' ) ); 
+        CRM_Core_Error::fatal( ts( 'This function is not implemented' ) );
     }
     function doExpressCheckout( &$params ) {
-        CRM_Core_Error::fatal( ts( 'This function is not implemented' ) ); 
+        CRM_Core_Error::fatal( ts( 'This function is not implemented' ) );
     }
 
     function doDirectPayment( &$params ) {
         CRM_Core_Error::fatal( ts( 'This function is not implemented' ) );
     }
 
-    /**  
+    /**
      * Main transaction function
-     *  
+     *
      * @param array $params  name value pair of contribution data
-     *  
-     * @return void  
-     * @access public 
-     *  
-     */   
-    function doTransferCheckout( &$params, $component ) 
+     *
+     * @return void
+     * @access public
+     *
+     */
+    function doTransferCheckout( &$params, $component )
     {
 
-        //doesn't look like these can actually be passed in....      
+        //doesn't look like these can actually be passed in....
         $config    = CRM_Core_Config::singleton( );
-        $cancelURL = $this->getCancelURL($component);     
-        $url = $config->userFrameworkResourceURL."extern/ipn.php?processor_name=nz.co.fuzion.payflowlink";
+        $cancelURL = $this->getCancelURL($component);
+        $url = CRM_Utils_System::url("civicrm/payment/ipn", "processor_name=payflowlink",false, null, false);
         $component = strtolower( $component );
         $paymentProcessorParams = $this->mapParamstoPaymentProcessorFields( $params,$component );
-        
+
         // Allow further manipulation of params via custom hooks
         CRM_Utils_Hook::alterPaymentProcessorParams( $this, $params, $paymentProcessorParams );
-                
+
         $processorURL = $this->_paymentProcessor['url_site'] ."?". $this->buildPaymentProcessorString($paymentProcessorParams);
         CRM_Utils_System::redirect( $processorURL) ;
-                 
-    }  
+
+    }
     /*
      * Get URL which the browser should be returned to if they cancel or are unsuccessful
      * @component string $omponent function is called from
      * @return string $cancelURL Fully qualified return URL
      * @todo Ideally this would be in the parent payment class
-     */  
+     */
     function getCancelURL($component){
         $component = strtolower( $component );
         if ( $component != 'contribute' && $component != 'event' ) {
@@ -148,16 +148,16 @@ class nz_co_fuzion_payflowlink extends CRM_Core_Payment {
         }
         if ( $component == 'event') {
             $cancelURL = CRM_Utils_System::url( 'civicrm/event/register',
-                                                "_qf_Confirm_display=true&qfKey={$params['qfKey']}", 
+                                                "_qf_Confirm_display=true&qfKey={$params['qfKey']}",
                                                 false, null, false );
         } else if ( $component == 'contribute' ) {
             $cancelURL = CRM_Utils_System::url( 'civicrm/contribute/transact',
-                                                "_qf_Confirm_display=true&qfKey={$params['qfKey']}", 
+                                                "_qf_Confirm_display=true&qfKey={$params['qfKey']}",
                                                 false, null, false );
-        }  
+        }
         return $cancelURL;
     }
-    
+
     /*
      * map the name / value set required by the payment processor
      * @param array $params
@@ -166,12 +166,12 @@ class nz_co_fuzion_payflowlink extends CRM_Core_Payment {
      function mapParamstoPaymentProcessorFields($params,$component){
        //AFAIK partner is always paypal but have configured it here so that it can be set in signature field if required
        $partner = (empty($this->_paymentProcessor['signature'] )) ? 'PAYPAL' : $this->_paymentProcessor['signature'];
-       
+
        $processorParams = array(
                                  'TYPE'        => 'S',
                                 'ADDRESS'     =>  $this->URLencodetoMaximumLength($params['street_address'],60),
                                 'CITY'        =>  $this->URLencodetoMaximumLength($params['city'],32),
-                                'LOGIN'       =>  $this->_paymentProcessor['user_name'],    
+                                'LOGIN'       =>  $this->_paymentProcessor['user_name'],
                                 'PARTNER'      =>  $partner,
                                 'AMOUNT'      =>  $params['amount'],
                                 'ZIP'          =>  $this->URLencodetoMaximumLength($params['postal_code'],10),
@@ -187,28 +187,28 @@ class nz_co_fuzion_payflowlink extends CRM_Core_Payment {
  //                              'TAX'//
                                 'STATE'        => $params['state_province'],
                                 'USER1'        => $params['contactID'],//USER fields are returned to Civi silent POST. Add them all in here for debug help.
-                                'USER2'        => $params['invoiceID'], 
-                                'USER3'        => CRM_Utils_Array::value('participantID',$params),      
-                                'USER4'        => CRM_Utils_Array::value('eventID',$params), 
-                                'USER5'        => CRM_Utils_Array::value( 'membershipID', $params ),    
-                                'USER6'        => CRM_Utils_Array::value( 'pledgeID', $params ), 
+                                'USER2'        => $params['invoiceID'],
+                                'USER3'        => CRM_Utils_Array::value('participantID',$params),
+                                'USER4'        => CRM_Utils_Array::value('eventID',$params),
+                                'USER5'        => CRM_Utils_Array::value( 'membershipID', $params ),
+                                'USER6'        => CRM_Utils_Array::value( 'pledgeID', $params ),
                                 'USER7'        => $component . ", " .  $params['qfKey'],
-                                'USER8'        => CRM_Utils_Array::value('contributionPageID',$params), 
+                                'USER8'        => CRM_Utils_Array::value('contributionPageID',$params),
                                 'USER9'        => CRM_Utils_Array::value('related_contact',$params),
-                                'USER10'      => CRM_Utils_Array::value( 'onbehalf_dupe_alert', $params ),                
+                                'USER10'      => CRM_Utils_Array::value( 'onbehalf_dupe_alert', $params ),
        );
       return $processorParams;
     }
-    
+
     /*
      * Encodes string for a URL & ensures that it does not exceed the maximum length of the relevant field
      * The cut needs to be made after spaces etc are transformed (as the string becomes longer after html encoding
-     * but must not include a partial transformed character 
+     * but must not include a partial transformed character
      * e.g. space is encoded to %20 - these 3 characters must be included or excluded but not partially included
-     * 
+     *
      * @params string $value value to be encoded
      * @params string @fieldLength maximum length of encoded field
-     * @return string $encodedString value html encoded 
+     * @return string $encodedString value html encoded
      */
     function URLencodetoMaximumLength($value,$fieldLength = 255)
     {
@@ -219,24 +219,32 @@ class nz_co_fuzion_payflowlink extends CRM_Core_Payment {
         }
         return $encodedString ;
     }
-    
+
     /*
      * Build string of name value pairs for submission to payment processor
-     * 
+     *
      * @params array $paymentProcessorParams
      * @return string $paymentProcessorString
      */
-    
+
     function buildPaymentProcessorString($paymentProcessorParams){
       $validParams = array();
       foreach ($paymentProcessorParams as $key => $value){
         if (!empty($value)){
-          $validParams[] = $key ."=".$value;   
+          $validParams[] = $key ."=".$value;
         }
       }
       $paymentProcessorString = implode('&',$validParams);
 
       return $paymentProcessorString;
-      
+
+    }
+
+    function handlePaymentNotification(){
+      require_once 'CRM/Core/Payment/PayflowLinkIPN.php';
+      $payFlowLinkIPN = new CRM_Core_Payment_PayflowLinkIPN( );
+      $payFlowLinkIPN ->main( );
+      //if for any reason we come back here
+      CRM_Core_Error::debug_log_message( "It should not be possible to reach this line" );
     }
 }
